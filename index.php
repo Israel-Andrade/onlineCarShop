1fa81069a7e35223216c686cd19e6dd4814cb27b
Nicholas Rosas <nrosas@csumb.edu>
2017-03-22 22:13:10 +0000

    Add file index.php with html skeleton as well as basic styles template in styles.css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+        <div id="wrapper">
+          <div id="header">
+              
+          </div>
+          <div id="content">
+              
+          </div>
+          <div id="footer">
+            <footer id="footer">
+            </footer>
+          </div>
+        </div>
+    </body>
+</html>
